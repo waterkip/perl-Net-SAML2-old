11e81bf6a739547e2c9ed299f803618d40dab7fc
Wesley Schwengle <wesley@schwengle.net>
2018-04-25 18:36:54 +0200

    style: Reorder Net::SAML2 layout

--- lib/Net/SAML2.pm
+++ lib/Net/SAML2.pm
@@ -4,66 +4,9 @@ use warnings;
 
 require 5.008_001;
 
-our $VERSION = '0.17';
-$VERSION = eval $VERSION;
+our $VERSION = '0.18';
 
-=head1 NAME
-
-Net::SAML2 - SAML bindings and protocol implementation
-
-=head1 SYNOPSIS
-
-  # generate a redirect off to the IdP:
-
-        my $idp = Net::SAML2::IdP->new($IDP);
-        my $sso_url = $idp->sso_url('urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect');
-        
-        my $authnreq = Net::SAML2::Protocol::AuthnRequest->new(
-                issuer        => 'http://localhost:3000/metadata.xml',
-                destination   => $sso_url,
-                nameid_format => $idp->format('persistent'),
-        )->as_xml;
-
-        my $redirect = Net::SAML2::Binding::Redirect->new(
-                key => 'sign-nopw-cert.pem',
-                url => $sso_url,
-        );
-
-        my $url = $redirect->sign($authnreq);
-
-  # handle the POST back from the IdP, via the browser:
-
-        my $post = Net::SAML2::Binding::POST->new;
-        my $ret = $post->handle_response(
-                $saml_response
-        );
-        
-        if ($ret) {
-                my $assertion = Net::SAML2::Protocol::Assertion->new_from_xml(
-                        xml => decode_base64($saml_response)
-                );
-
-                # ...
-        }
-
-=head1 DESCRIPTION
-
-Support for the Web Browser SSO profile of SAML2. 
-
-This is a very early release, but one which will correctly perform the
-SSO process.
-
-=head1 MAJOR CAVEATS
-
-=over
-
-=item SP-side protocol only
-
-=item Requires XML metadata from the IdP
-
-=back
-
-=cut
+# ABSTRACT: SAML bindings and protocol implementation
 
 # entities
 use Net::SAML2::IdP;
@@ -81,7 +24,62 @@ use Net::SAML2::Protocol::LogoutResponse;;
 use Net::SAML2::Protocol::Assertion;
 use Net::SAML2::Protocol::ArtifactResolve;
 
-=pod
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+    # generate a redirect off to the IdP:
+    my $idp = Net::SAML2::IdP->new($IDP);
+    my $sso_url = $idp->sso_url('urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect');
+
+    my $authnreq = Net::SAML2::Protocol::AuthnRequest->new(
+        issuer        => 'http://localhost:3000/metadata.xml',
+        destination   => $sso_url,
+        nameid_format => $idp->format('persistent'),
+    )->as_xml;
+
+    my $redirect = Net::SAML2::Binding::Redirect->new(
+        key => 'sign-nopw-cert.pem',
+        url => $sso_url,
+    );
+
+    my $url = $redirect->sign($authnreq);
+
+    # handle the POST back from the IdP, via the browser:
+
+    my $post = Net::SAML2::Binding::POST->new;
+    my $ret = $post->handle_response(
+        $saml_response
+    );
+
+    if ($ret) {
+        my $assertion = Net::SAML2::Protocol::Assertion->new_from_xml(
+                xml => decode_base64($saml_response)
+        );
+
+        # ...
+    }
+
+=head1 DESCRIPTION
+
+Support for the Web Browser SSO profile of SAML2.
+
+This is a very early release, but one which will correctly perform the
+SSO process.
+
+=head1 MAJOR CAVEATS
+
+=over
+
+=item SP-side protocol only
+
+=item Requires XML metadata from the IdP
+
+=back
+
+=cut
 
 =head1 AUTHOR
 
@@ -102,4 +100,3 @@ it under the same terms as Perl itself.
 
 =cut
 
-1;
